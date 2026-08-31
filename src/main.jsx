0252a6a9651a3e2f882f0d8b8f6aaa1472848f2b
eaa24sikh <eaa24sikh@students.eaaa.dk>
2026-08-31 11:24:50 +0200

    footer
    sat footer ind i sin egen components så den ikke skal skrivet på hver side

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles.css";
 import App from "./App.jsx";
+import Footer from "./components/Footer.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
+      <Footer></Footer>
     </BrowserRouter>
   </StrictMode>
 );
